fa5234bd259459f9a0a89b334ecf0bc9f1cecbec
jeffason1997 <jeffreylantinga@hotmail.com>
2018-11-08 10:29:29 +0100

    Last Changes

--- Opdrachten_Embedded_Software/Opdrachten_Embedded_Software/Les1.c
+++ Opdrachten_Embedded_Software/Opdrachten_Embedded_Software/Les1.c
@@ -1,24 +1,50 @@
-// Opdrachten_Embedded_Software.c : Defines the entry point for the console application.
-//
-
+/*
+* BinairyClock_Microcontrollers.c
+*
+* Created: 8-3-2018 14:37:26
+* Author : jeffr
+*/
 #include "stdafx.h"
-#include <string.h>
+#include <time.h>
+#pragma warning(disable : 4996)
 
+#define F_CPU 8000000
 
-char geefLaatsteChar(char  woord[]) {
-	char laatsteLetter;
-	laatsteLetter = woord[strlen(woord) - 1];
-	return laatsteLetter;
+unsigned int_to_int(unsigned k) {
+	if (k == 0) return 0;
+	if (k == 1) return 1;                       /* optional */
+	return (k % 2) + 10 * int_to_int(k / 2);
 }
 
 int main()
 {
-	char mijnWoord[10] = "Yeah Boi";
-	printf("%s\n", mijnWoord);
-	printf("De laatste letter van mijnWoord is %c\n", geefLaatsteChar(mijnWoord));
+	time_t rawtime;
+	struct tm * timeinfo;
 
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	printf("Current local time and date: %d", timeinfo->tm_min);
+
+		time(&rawtime);
+		timeinfo = localtime(&rawtime);
+
+		int hour1 = timeinfo->tm_hour / 10;
+		int hour2 = timeinfo->tm_hour % 10;
+		int min1 = timeinfo->tm_min / 10;
+		int min2 = timeinfo->tm_min % 10;
+		int sec1 = timeinfo->tm_sec / 10;
+		int sec2 = timeinfo->tm_sec % 10;
+		
+
+		printf(" %d", int_to_int(hour1));
+		printf(" %d", int_to_int(hour2));
+		printf(" %d", int_to_int(min1));
+		printf(" %d", int_to_int(min2));
+		printf(" %d", int_to_int(sec1));
+		printf(" %d", int_to_int(sec2));
+	
+		while (1) {};
 
 	return 0;
 }
-
 
